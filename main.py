59979b32aa385e3b441673c846311cfaff5d245d
Qui Ego <acct.qui@gmail.com>
2015-11-04 20:12:42 +0800

    made it five boxes

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import turtle
 
 wn = turtle.Screen()
+tim = turtle.Turtle()
 terry = turtle.Turtle()
-tyler = turtle.Turtle()
+tanya = turtle.Turtle()
 taylor = turtle.Turtle()
+tony = turtle.Turtle()
 
 for a in range(4):
+    tim.forward(12.5)
+    tim.left(90)
     terry.forward(25)
     terry.left(90)
-    tyler.forward(50)
-    tyler.left(90)
+    tanya.forward(50)
+    tanya.left(90)
     taylor.forward(100)
     taylor.left(90)
+    tony.forward(200)
+    tony.left(90)
 
 wn.mainloop()
