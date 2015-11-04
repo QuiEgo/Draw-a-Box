d976ea411f08385cb247b12a00f0c4e5bd45a836
Qui Ego <acct.qui@gmail.com>
2015-11-04 20:03:10 +0800

    Used the turtle library to draw 4 squares of varying size.

---
+++ main.py
@@ -0,0 +1,16 @@
+import turtle
+
+wn = turtle.Screen()
+terry = turtle.Turtle()
+tyler = turtle.Turtle()
+taylor = turtle.Turtle()
+
+for a in range(4):
+    terry.forward(25)
+    terry.left(90)
+    tyler.forward(50)
+    tyler.left(90)
+    taylor.forward(100)
+    taylor.left(90)
+
+wn.mainloop()
